af2a2f61571adb78245b322a0c51b5046d569314
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-03-05 10:44:22 +0900

    solve: b0j-10998

---
+++ src/baekjoon/bronze/boj_10998.java
@@ -0,0 +1,14 @@
+package baekjoon.bronze;
+
+import java.util.Scanner;
+
+public class boj_10998 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(a*b);
+    }
+}
