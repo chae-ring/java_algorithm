37b3feb75af4d43a959604dc7574cb46fa08518b
chae-ring <149919388+chae-ring@users.noreply.github.com>
2026-03-05 11:10:08 +0900

    solve: b0j-18108

---
+++ src/baekjoon/bronze/boj_18108.java
@@ -0,0 +1,12 @@
+package baekjoon.bronze;
+
+import java.io.*;
+
+public class boj_18108 {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int year = Integer.parseInt(br.readLine());
+        System.out.println(year - 543);
+    }
+}
